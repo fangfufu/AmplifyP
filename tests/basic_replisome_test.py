3a775b5f7bc1889aad5da487497b82d472b7cfdb
Fufu Fang <fangfufu@fangfufu.co.uk>
2023-10-09 03:18:12 +0100

    verified replicon slicing

--- tests/basic_replisome_test.py
+++ tests/basic_replisome_test.py
@@ -5,8 +5,8 @@ from amplifyp.dna import DNA, DNAType
 from amplifyp.replisome import Replisome
 
 
-def test_replisome_replicon_slice_limit_stop() -> None:
-    """Test replicon slicing at the end of the index limit."""
+def test_replisome_linear_replicon_slice_limit_stop() -> None:
+    """Test replicon slicing for linear DNA at the end of the index limit."""
     target: DNA = DNA("GTGTACTCAGTTCCATAAACGAGCTATTAGATATGAGGTCCGTAGATTGAAAAGGGTGA")
     primer: DNA = DNA("GTGTACT", DNAType.PRIMER)
     target_str: str = target[0 : len(primer)].sequence[::-1] + len(primer) * "-"
@@ -17,3 +17,17 @@ def test_replisome_replicon_slice_limit_stop() -> None:
         test_str: str = replisome.target[replisome.replicon_slice(test_index)].sequence
         assert isinstance(test_str, str)
         assert test_str == target_str[len(primer) - i : 2 * len(primer) - i]
+
+
+def test_replisome_linear_replicon_slice_limit_start() -> None:
+    """Test replicon slicing for linear DNA at the start of the index limit."""
+    target: DNA = DNA("ATTGTGCGATCCCTGCACCTCAGCTAAGGTAGCTACCAATATTTAGTTTCTAAGCCTTGC")
+    primer: DNA = DNA("ATTGTGCGAT", DNAType.PRIMER)
+    target_str: str = len(primer) * "-" + target.sequence[-len(primer) : :][::-1]
+    assert isinstance(target_str, str)
+    for i in range(0, len(primer)):
+        replisome = Replisome(target, primer, min_overlap=i)
+        test_index = replisome.target_index_limit.start
+        test_str: str = replisome.target[replisome.replicon_slice(test_index)].sequence
+        assert isinstance(test_str, str)
+        assert test_str == target_str[i : len(primer) + i]
