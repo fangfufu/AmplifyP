ced00a7d3a3b5e6e3a747a029bb82a6ccba14ddd
Fufu Fang <fangfufu@fangfufu.co.uk>
2024-01-29 04:54:43 +0000

    added a few more linear search examples

--- tests/primer_search_test.py
+++ tests/primer_search_test.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Test AmplifyP implementation using real life examples."""
+"""Test primer search using Amplify 4 built-in examples."""
 
 import logging
 
@@ -52,30 +52,70 @@ amplify4_template = DNA(
 )
 
 primer_1423 = Primer("GCCAAAGTGTGTTGGC", "1423")
+primer_10290 = Primer("GTGGGTATCACAAATTTGGG", "10290")
 primer_20049 = Primer("CAGATGTGGACATAAAAAGACAAT", "20049")
+primer_2223 = Primer("CGTCCGCACACAACCTTTCC", "2223")
 
 
-def test_search_primer_20049() -> None:
+def test_linear_search_primer_10290() -> None:
+    """Test: search for primer 10290 in the Amplify 4 example template."""
+    repliconf = Repliconf(amplify4_template, primer_10290, Settings)
+    repliconf.search()
+    origin_id = repliconf.origin_id
+    logging.info(origin_id)
+
+    origin_1 = repliconf.origin(DNADirection.FWD, origin_id[DNADirection.FWD][0])
+    assert origin_1.primability == 1.0
+    assert origin_1.stability == 1.0
+    assert origin_1.quality == 1.0
+
+    origin_2 = repliconf.origin(DNADirection.FWD, origin_id[DNADirection.FWD][1])
+    assert origin_2.primability == 0.8059701492537313
+    assert origin_2.stability == 0.4717741935483871
+    assert origin_2.quality == 0.09718042850264788
+
+
+def test_linear_search_primer_20049() -> None:
     """Test: search for primer 20049 in the Amplify 4 example template."""
     repliconf = Repliconf(amplify4_template, primer_20049, Settings)
     repliconf.search()
     origin_id = repliconf.origin_id
-    print(origin_id)
+    logging.info(origin_id)
 
     origin_1 = repliconf.origin(DNADirection.FWD, origin_id[DNADirection.FWD][0])
     logging.info(origin_1)
-    logging.info(origin_1.primability)
-    logging.info(origin_1.stability)
-    logging.info(origin_1.quality)
     assert origin_1.primability == 0.8188405797101449
     assert origin_1.stability == 0.4838709677419355
     assert origin_1.quality == 0.12838943431510044
 
     origin_2 = repliconf.origin(DNADirection.REV, origin_id[DNADirection.REV][0])
     logging.info(origin_2)
-    logging.info(origin_2.primability)
-    logging.info(origin_2.stability)
-    logging.info(origin_2.quality)
     assert origin_2.primability == 0.855072463768116
     assert origin_2.stability == 0.9005376344086021
     assert origin_2.quality == 0.6945126227208976
+
+
+def test_linear_search_primer_2223() -> None:
+    """Test: search for primer 2223 in the Amplify 4 example template."""
+    repliconf = Repliconf(amplify4_template, primer_2223, Settings)
+    repliconf.search()
+    origin_id = repliconf.origin_id
+    logging.info(origin_id)
+
+    origin_1 = repliconf.origin(DNADirection.FWD, origin_id[DNADirection.FWD][0])
+    logging.info(origin_1)
+    assert origin_1.primability == 1.0
+    assert origin_1.stability == 1.0
+    assert origin_1.quality == 1.0
+
+    origin_2 = repliconf.origin(DNADirection.REV, origin_id[DNADirection.REV][0])
+    logging.info(origin_2)
+    assert origin_2.primability == 0.8208955223880597
+    assert origin_2.stability == 0.4306451612903226
+    assert origin_2.quality == 0.06442585459797757
+
+    origin_2 = repliconf.origin(DNADirection.REV, origin_id[DNADirection.REV][1])
+    logging.info(origin_2)
+    assert origin_2.primability == 0.8208955223880597
+    assert origin_2.stability == 0.5419354838709678
+    assert origin_2.quality == 0.2035387578237841
