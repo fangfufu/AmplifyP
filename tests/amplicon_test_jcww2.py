4180fe2e61fa4a675dd878930c411d085e17c788
Fufu Fang <fangfufu@fangfufu.co.uk>
2026-01-20 02:58:39 +0000

    Configure Coverage Gutter

---
+++ tests/amplicon_test_jcww2.py
@@ -0,0 +1,6 @@
+"""Comprehensive test for amplicon using examples provided by jcww2."""
+
+
+def test_jcww2_amplicon_examples() -> None:
+    """Test amplicon creation using examples provided by jcww2."""
+    return
