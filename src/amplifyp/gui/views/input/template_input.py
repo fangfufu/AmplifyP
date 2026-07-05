d8cee1049aec29958f53305ee4c169c0dd4e1c94
Fufu Fang <fangfufu@fangfufu.co.uk>
2026-07-05 21:57:58 +0100

    refactor(gui): reuse clean_sequence helper for gutter
    Simplify sequence length calculation in line numbers gutter by reusing
    the clean_sequence utility helper.
    
    Addresses review comment r3525617084.

--- src/amplifyp/gui/views/input/template_input.py
+++ src/amplifyp/gui/views/input/template_input.py
@@ -367,12 +367,7 @@ class TemplateInput(ft.Container):  # type: ignore[misc]
         current_idx = 1
         for line in lines:
             line_indices.append(str(current_idx))
-            current_idx += len(
-                line.replace("\\n", "")
-                .replace("\\t", "")
-                .replace("\\r", "")
-                .translate(str.maketrans("", "", " \t\n\r\v\f"))
-            )
+            current_idx += len(clean_sequence(line))
 
         self.line_numbers_text.value = "\n".join(line_indices)
 
